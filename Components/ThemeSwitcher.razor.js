60c28c0064342912a4a4a4a7353e03105880cc72
Örvar Karlsson <orvar@secure-aid.com>
2024-03-02 16:42:46 +0100

    deleted page

--- Components/ThemeSwitcher.razor.js
+++
@@ -1,7 +0,0 @@
-export function themeToggle(isDarkMode) {
-
-    var links = document.getElementsByTagName('link');
-    var lastLink = links[links.length - 1];
-
-    lastLink.disabled = isDarkMode ? false : true;
-}
