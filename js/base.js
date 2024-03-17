0e676b4f35a78a5db3ae64b462309fb08ae229ad
Örvar Karlsson <orvar@secure-aid.com>
2024-03-17 19:26:00 +0100

    Page deleted

--- js/base.js
+++
@@ -1,9 +0,0 @@
-﻿var path = window.location.pathname.split('/');
-var base = document.getElementsByTagName('base')[0];
-if (window.location.host.includes('localhost')) {
-    base.setAttribute('href', '/');
-} else if (path.length > 2) {
-    base.setAttribute('href', 'https://icehub82.github.io/' + path[1] + '/');
-} else if (path[path.length - 1].length != 0) {
-    window.location.replace(window.location.origin + window.location.pathname + '/' + window.location.search);
-}
